ed34649029e4003c9b22ce35327499fe70e4f020
Hariom dangi <hdangi224@gmail.com>
2025-01-11 18:46:51 +0530

    solve bugs on production

--- backend/utils/SendToken.js
+++ backend/utils/SendToken.js
@@ -3,6 +3,8 @@ const sendToken = (user, statusCode, res) => {
   const option = {
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
     httpOnly: true,
+    secure: true, 
+    sameSite: "None", 
   };
 
   res
--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BaseURL = "http://localhost:8080/";
+// export const BaseURL = "http://localhost:8080/";
+export const BaseURL = "https://task-vistaar-e-com-backend.vercel.app/";
 
 const instance = axios.create({
   baseURL: BaseURL,
